31359d47e0f8a5c585cc323fddca33cf485c8b6e
rajeshhariharan1810 <rajesh.hariharan1810@gmail.com>
2025-11-06 14:58:49 -0500

    hi

--- NumberLineFractions.py
+++ NumberLineFractions.py
@@ -1,37 +1,57 @@
 from fractions import Fraction
 
+def calculate(num1, num2, num_points):
+    D = num2 - num1
+    d = D/(num_points + 1)
+
+    points = []
+    temp_point = num1
+
+    for i in range(num_points):
+        points.append(temp_point + d)
+        temp_point += d
+
+    return points
+
+def number1(num1):
+    if num1.find("/") == -1:
+        return num1
+    else:
+        num1Whole = int(num1[:num1.index(" ")])
+        num1Num = int(num1[num1.index(" ") + 1 :num1.index("/")])
+        num1Den = int(num1[num1.index("/") + 1:])
+
+        if num1Whole < 0:
+            num1 = Fraction(num1Whole.__abs__() * num1Den + num1Num, num1Den) * -1
+            return num1
+        else:
+            num1 = Fraction(num1Whole * num1Den + num1Num, num1Den)
+            return num1
+
+def number2(num2):
+    if num2.find("/") == -1:
+        return num2
+    else:
+        num2Whole = int(num2[:num2.index(" ")])
+        num2Num = int(num2[num2.index(" ") + 1:num2.index("/")])
+        num2Den = int(num2[num2.index("/") + 1:])
+
+        if num2Whole < 0:
+            num2 = Fraction(num2Whole.__abs__() * num2Den + num2Num, num2Den) * -1
+            return num2
+        else:
+            num2 = Fraction(num2Whole * num2Den + num2Num, num2Den)
+            return num2
+
 str_num1 = input("Start Point: ")
 str_num2 = input("End Point: ")
-num_points = int(input("Number of Points: "))
+numPoints = int(input("Number of Points: "))
 
-num1Whole = int(str_num1[:str_num1.index(" ")])
-num1Num = int(str_num1[str_num1.index(" ") + 1 :str_num1.index("/")])
-num1Den = int(str_num1[str_num1.index("/") + 1:])
-
-num2Whole = int(str_num2[:str_num2.index(" ")])
-num2Num = int(str_num2[str_num2.index(" ") + 1 :str_num2.index("/")])
-num2Den = int(str_num2[str_num2.index("/") + 1:])
-
-if num1Whole < 0:
-    num1 = Fraction(num1Whole.__abs__() * num1Den + num1Num, num1Den) * -1
-else:
-    num1 = Fraction(num1Whole * num1Den + num1Num, num1Den)
-if num2Whole < 0:
-    num2 = Fraction(num2Whole.__abs__() * num2Den + num2Num, num2Den) * -1
-else:
-    num2 = Fraction(num2Whole * num2Den + num2Num, num2Den)
-
-D = num2 - num1
-d = D/(num_points + 1)
-
-points = []
-temp_point = num1
-
-for i in range(num_points):
-    points.append(temp_point + d)
-    temp_point += d
+no1 = number1(str_num1)
+no2 = number2(str_num2)
+answer = calculate(no1, no2, numPoints)
 
 x = 1
-for i in range(num_points):
-    print(f"Point {x}: " , points[i])
+for j in range(numPoints):
+    print(f"Point {x}: ", answer[j])
     x += 1
