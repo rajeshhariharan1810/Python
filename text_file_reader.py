7a5721d79c36265d1c0555554396a4c9adc7901e
rajeshhariharan1810 <rajesh.hariharan1810@gmail.com>
2025-11-15 20:57:11 -0500

    Renamed files properly

--- text_file_reader.py
+++ text_file_reader.py
@@ -1,5 +1,5 @@
 wordCount = {}
-with open('W1Act1.txt', 'r') as file:
+with open('sample_text_file.txt', 'r') as file:
     for line in file:
         line = line.strip()
         wordCount[line] = wordCount.get(line, 0) + 1
